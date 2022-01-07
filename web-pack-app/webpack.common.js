7e91240a1f347853c7a03703210c8e29f1a22c22
unknown <ricardorxz82@gmail.com>
2022-01-07 11:32:49 -0700

    Added dev and prod configs

--- web-pack-app/webpack.common.js
+++ web-pack-app/webpack.common.js
@@ -1,20 +1,14 @@
-const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
-    mode: "development",
-    output: {
-        filename: "main.[contenthash].js",
-        path: path.resolve(__dirname, "dist")
-    },
     module:
     {
         rules: [
             {
                 test: /\.scss$/,
                 use: [
-                    'style-loader', 
+                    'style-loader',
                     'css-loader',
                     'sass-loader'
                 ]
